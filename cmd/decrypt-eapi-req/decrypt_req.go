dc82fbcf010004ca7d391bcb559506f9f4dfe1ed
Jixun Wu <Jixun.Moe@gmail.com>
2020-07-06 20:21:35 +0100

    Add command to decrypt request/response traffic from EAPI client.

---
+++ cmd/decrypt-eapi-req/decrypt_req.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"github.com/JixunMoe/netease-api-go/NetEaseAPI/crypto"
+	"log"
+	"os"
+)
+
+func main() {
+	argv := os.Args
+	l := log.New(os.Stderr, "", 0)
+
+	if len(argv) <= 1 {
+		l.Printf("%s: Request packet decryptor.\n", argv[0])
+		l.Printf("Usage:\n")
+		l.Printf("%s <request body in hex or b64 str>", argv[0])
+		l.Printf("\n")
+		return
+	}
+
+	payload, err := hex.DecodeString(argv[1])
+	if err != nil {
+		payload, err = base64.StdEncoding.DecodeString(argv[1])
+		if err != nil {
+			l.Println("could not decode argument (hex/base64)")
+			os.Exit(1)
+			return
+		}
+	}
+
+	req, err := crypto.DecryptEAPIRequestBody(payload)
+	if err != nil {
+		l.Println("could not decrypt data")
+		os.Exit(1)
+		return
+	}
+
+	fmt.Printf("Path: %s\n", req.Path)
+	fmt.Printf("Data: %s\n", req.Data)
+}
---
+++ cmd/decrypt-eapi-resp/decrypt_resp.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"github.com/JixunMoe/netease-api-go/NetEaseAPI/crypto"
+	"log"
+	"os"
+)
+
+func main() {
+	argv := os.Args
+	l := log.New(os.Stderr, "", 0)
+
+	if len(argv) <= 1 {
+		l.Printf("%s: Response packet decryptor.\n", argv[0])
+		l.Printf("Usage:\n")
+		l.Printf("%s <response body in hex or b64 str>", argv[0])
+		l.Printf("\n")
+		return
+	}
+
+	payload, err := hex.DecodeString(argv[1])
+	if err != nil {
+		payload, err = base64.StdEncoding.DecodeString(argv[1])
+		if err != nil {
+			l.Println("could not decode argument (hex/base64)")
+			os.Exit(1)
+			return
+		}
+	}
+
+	resp := crypto.DecryptEAPIResponse(payload)
+	if err != nil {
+		l.Println("could not decrypt data")
+		os.Exit(1)
+		return
+	}
+
+	fmt.Printf("Size: %d\n", len(resp))
+	fmt.Println(string(resp))
+}
